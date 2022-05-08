4f9de1b12cbe3272e2d25e229ee202eb30272cc6
Lilitha Mdlalana <jranonymous517@gmail.com>
2022-05-08 18:56:02 +0200

    Add user authentication and update title blocks

--- btre/urls.py
+++ btre/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('listings/',include('listings.urls')),
     path('',include('pages.urls')),
+    path('accounts/',include('accounts.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
